7fd7ef27051ac5b1cb50643bab56c43c8c1623ed
unknown <ouldeabdellatif40@gmail.com>
2024-09-26 07:48:13 +0100

    created a job to test node-schedule

---
+++ index.js
@@ -0,0 +1,13 @@
+const schedule = require('node-schedule');
+const http    = require('http')
+
+// real time '*/20 * * * *'
+let i = 0;
+let n = 3;
+const job = schedule.scheduleJob('*/2 * * * * *', ()=>{
+    console.count("test")
+    i++;
+    if (i === n) {
+        job.cancel()
+    }
+})
